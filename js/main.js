9af155052e0cfa2a85951dfc0ec106115be8aa17
eric diskin <ericdiskin26@gmail.com>
2019-04-15 22:13:56 -0400

    broke things

--- js/main.js
+++ js/main.js
@@ -8,75 +8,91 @@ function Camera(w,h) {
     this.height = h;
 }
 
-function Map() {}
-Map.prototype = {
-    rows: window.innerWidth / 16,
-    cols: window.innerHeight / 16,
-    size: 7,
-    x: 0,
-    y: 0,
-    map: [],
-    generate: function () {
-        this.rows = window.innerWidth / this.size;
-        this.cols = window.innerHeight / this.size;
+function Map(r,c,s=7) {
+    this.rows = r;
+    this.cols = c;
+    this.size = s;
+    this.x = 0;
+    this.y = 0;
+    this.map = []
+    this.generate = function () {
         this.map = [];
-        for (var i = 0; i < this.cols * this.rows;i++) {
-            this.map.push(Math.floor(Math.random() * 5));
+        for (var i = 0; i < this.cols * this.rows; i++) {
+            this.map.push(Math.round(Math.random()));
         }
-    },
-    get: function (x,y) {
-        return this.map[Math.round(x)*Math.round(y)]
-    },
-    render: function (c,camera) {
-        this.rows = window.innerHeight / this.size / 2;
-        this.cols = window.innerWidth / this.size / 2;
-        for (let y = 0; y < this.rows; y++) {
-            for (let x = 0; x < this.cols; x++) {
+    }
+    this.get = function (x, y) {
+        return this.map[Math.floor(x + 1) * Math.floor(y + 1) - Math.floor(y) - Math.floor(x)]
+    }
+    this.render = function (c, camera) {
+        var startCol = Math.round(camera.x) / this.size,
+            startRow = Math.round(camera.y) / this.size,
+            endCol = ((camera.width) / this.size) - startCol,// / this.size,
+            endRow = ((camera.height) / this.size) - startRow// / this.size;
+
+            for (let y = startRow; y < endRow; y++) {
+                for (let x = startCol; x < endCol; x++) {
                 c.beginPath();
-                switch(this.get(x,y)) {
-                case 0:
-                    c.fillStyle = "rgb(255,0,0)";
-                    break;
-                case 1:
-                    c.fillStyle = "rgb(0,255,0)";
-                    break;
-                case 2:
-                    c.fillStyle = "rgb(0,0,255)"
-                    break;
-                case 3:
-                    c.fillStyle = "rgb(0,0,0)"
-                    break;
-                case 4:
-                    c.fillStyle = "rgb(255,255,255)"
-                    break;
+
+                switch (this.get(x, y)) {
+                    case 0:
+                        c.fillStyle = "rgb(255,0,0)";
+                        break;
+                    case 1:
+                        c.fillStyle = "rgb(0,255,0)";
+                        break;
+                    default:
+                        c.fillStyle = "rgb(0,0,255)";
+                        break;
                 }
+
                 c.fillRect(x * this.size + camera.x, y * this.size + camera.y, this.size, this.size);
-                c.strokeRect(x * this.size + camera.x, y * this.size + camera.y, this.size, this.size);
+                //c.strokeRect(x * this.size + camera.x, y * this.size + camera.y, this.size, this.size);
                 c.closePath();
             }
         }
+        c.beginPath();
+        c.fillStyle = "rgb(0,0,0)";
+        c.fillRect(canv.width / 2 + 4, canv.height / 2 + 5.1,this.size,this.size);
+        c.closePath();
     }
 }
+
 function render() {
-    map.generate();
+    //map.generate();
     map.render(c, cam)
 }
+
 $(window).load(function () {
     canv = $("canvas")[0];
     c = canv.getContext("2d");
 
-    canv.width = window.innerWidth / 2;
-    
-    canv.height = window.innerHeight / 2;
-    
-    map = new Map();
+    canv.width = 600
+    canv.height = 500
+    var s = 1
+    map = new Map(10,10,s);
     map.generate();
-    console.log(map);
-    console.log(map.get(0,1));
-    cam = new Camera(window.innerWidth / 2, window.innerHeight /2 );
+    cam = new Camera(canv.width,canv.height);
+    console.log("MAP generated");
+    
     setInterval(() => {
         exec("_start_contxt,'canvas',;clr,;_end_contxt,;")
         render()
-    }, 100);
+    }, 20);
 
+    $(window).keydown(function (e) {
+        var kc = e.keyCode;
+        if (kc == 38) {
+            cam.y += s
+        }
+        if (kc == 40) {
+            cam.y -= s
+        }
+        if (kc == 39) {
+            cam.x -= s
+        }
+        if (kc == 37) {
+            cam.x += s
+        }
+    })
 })
